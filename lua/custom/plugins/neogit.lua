58a0b15dd129ca4ba3439ada059a9c66fef4e337
Alejandro Sanchez <Alejandro_Sanchez@eogresources.com>
2024-12-12 14:26:43 -0600

    added neogit

---
+++ lua/custom/plugins/neogit.lua
@@ -0,0 +1,18 @@
+return {
+  'NeogitOrg/neogit',
+  dependencies = {
+    'nvim-lua/plenary.nvim', -- required
+    'sindrets/diffview.nvim', -- optional - Diff integration
+
+    -- Only one of these is needed.
+    'nvim-telescope/telescope.nvim', -- optional
+    -- 'ibhagwan/fzf-lua', -- optional
+    -- 'echasnovski/mini.pick', -- optional
+  },
+  config = function()
+    local neogit = require 'neogit'
+    neogit.setup {}
+
+    vim.keymap.set('n', '<leader>go', '<cmd>Neogit<cr>', { desc = 'Open NeoGit' })
+  end,
+}
