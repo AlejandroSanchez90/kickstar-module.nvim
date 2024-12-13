5b3388dec9a9543122b4bb7821c0804acdf62d0c
Alejandro Sanchez <contact@alejandrosanchez.dev>
2024-12-12 22:30:59 -0600

    updated treesitter

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -2,21 +2,70 @@ return {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
+    dependencies = {
+      'windwp/nvim-ts-autotag',
+      'MeanderingProgrammer/render-markdown.nvim',
+      'nvim-tree/nvim-web-devicons',
+      'norcalli/nvim-colorizer.lua',
     },
+    config = function()
+      local treesitter = require 'nvim-treesitter.configs'
+      require('colorizer').setup {
+        '*',
+      }
+      require('nvim-ts-autotag').setup {
+        opts = {
+          enable_close = true,
+          enable_rename = true,
+          enable_close_on_slash = false,
+        },
+      }
+      require('render-markdown').setup {}
+      treesitter.setup { -- enable syntax highlighting
+        ignore_install = {},
+        sync_install = false,
+        auto_install = false,
+        modules = {},
+        highlight = {
+          enable = true,
+        },
+        -- enable indentation
+        indent = { enable = true },
+        -- enable autotagging (w/ nvim-ts-autotag plugin)
+        -- ensure these language parsers are installed
+        ensure_installed = {
+          'json',
+          'javascript',
+          'typescript',
+          'tsx',
+          'yaml',
+          'html',
+          'css',
+          'prisma',
+          'markdown',
+          'markdown_inline',
+          -- "svelte",
+          -- "graphql",
+          'bash',
+          'lua',
+          'vim',
+          'dockerfile',
+          'gitignore',
+          'query',
+          'vimdoc',
+          'c',
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = '<C-space>',
+            node_incremental = '<C-space>',
+            scope_incremental = false,
+            node_decremental = '<bs>',
+          },
+        },
+      }
+    end,
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
     --
