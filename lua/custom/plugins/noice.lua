3ab9c97ea8d9e6c025114f981f01ec1daa1a7519
Alejandro Sanchez <contact@alejandrosanchez.dev>
2024-12-14 16:24:32 -0600

    noice lsp progress

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -14,10 +14,11 @@ return {
       max_width = 50,
       top_down = false,
     }
+
     require('noice').setup {
       lsp = {
         progress = {
-          enabled = false,
+          enabled = true,
         },
         signature = {
           enabled = true,
@@ -27,15 +28,12 @@ return {
             trigger = true,
           },
         },
-
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
         override = {
           ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
           ['vim.lsp.util.stylize_markdown'] = true,
           ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
         },
       },
-      -- you can enable a preset for easier configuration
       presets = {
         bottom_search = true, -- use a classic bottom cmdline for search
         command_palette = true, -- position the cmdline and popupmenu together
--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -24,7 +24,6 @@ return {
 
       -- Useful status updates for LSP.
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
 
       -- Allows extra capabilities provided by nvim-cmp
       'hrsh7th/cmp-nvim-lsp',
