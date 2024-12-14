1c431a3030b0d668313d8d6c7bd052555ad549f3
Alejandro Sanchez <contact@alejandrosanchez.dev>
2024-12-14 10:13:59 -0600

    diff view open

--- lua/custom/plugins/neogit.lua
+++ lua/custom/plugins/neogit.lua
@@ -16,5 +16,6 @@ return {
     vim.keymap.set('n', '<leader>go', '<cmd>Neogit<cr>', { desc = 'Open NeoGit' })
     vim.keymap.set('n', '<leader>gdl', '<cmd>DiffviewFileHistory % --base=LOCAL<cr>', { desc = 'Document Local Diff' })
     vim.keymap.set('n', '<leader>gdh', '<cmd>DiffviewFileHistory % <cr>', { desc = 'Document History Last Commit' })
+    vim.keymap.set('n', '<leader>gh', '<cmd>DiffviewOpen<cr>', { desc = 'Status' })
   end,
 }
