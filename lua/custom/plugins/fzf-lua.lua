a82b70dac8e8cf08d4ac596811aa013016b6c2b8
Alejandro Sanchez <contact@alejandrosanchez.dev>
2024-12-14 09:00:12 -0600

    removed telescope for fzflua

---
+++ lua/custom/plugins/fzf-lua.lua
@@ -0,0 +1,41 @@
+return {
+  'ibhagwan/fzf-lua',
+  -- optional for icon support
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    -- calling `setup` is optional for customization
+    local fzflua = require 'fzf-lua'
+    fzflua.setup {}
+
+    local keymap = vim.keymap -- for conciseness
+
+    function SendBuffersToQuickfix()
+      local bufs = vim.api.nvim_list_bufs()
+      local qflist = {}
+
+      for _, buf in ipairs(bufs) do
+        table.insert(qflist, { bufnr = buf })
+      end
+
+      vim.fn.setqflist(qflist, 'r')
+      fzflua.grep_quickfix()
+    end
+
+    keymap.set('n', '<leader>ft', '<cmd>:lua SendBuffersToQuickfix()<CR>', { desc = 'Grep open buffers' })
+    keymap.set('n', '<leader>ff', '<cmd>FzfLua files --keep-right<cr>', { desc = 'Find files in cwd' })
+    keymap.set('n', '<leader>fdd', '<cmd>FzfLua diagnostics_document<cr>', { desc = 'Diagnostic Document' })
+    keymap.set('n', '<leader>fdw', '<cmd>FzfLua diagnostics_workspace<cr>', { desc = 'Diagnostic Workspace' })
+    keymap.set('n', '<leader>fh', '<cmd>FzfLua oldfiles<cr>', { desc = 'Find recent files' })
+    keymap.set('n', '<leader>fs', '<cmd>FzfLua live_grep_glob --keep-right<cr>', { desc = 'Find string in cwd' })
+    keymap.set('n', '<leader>fc', '<cmd>FzfLua grep_cword rg_glob=true<cr>', { desc = 'Find string under cursor in cwd' })
+    keymap.set('n', '<leader>fb', '<cmd>FzfLua buffers sort_lastused=false<cr>', { desc = 'Find buffers' })
+    keymap.set('n', '<leader><leader>', '<cmd>FzfLua buffers sort_lastused=true<cr>', { desc = 'Find buffers' })
+    keymap.set('n', '<leader>fr', '<cmd>FzfLua resume<cr>', { desc = 'Find resume' })
+    keymap.set('n', '<leader>fq', '<cmd>FzfLua quickfix<cr>', { desc = 'Find quickfix' })
+    keymap.set('n', '<leader>fg', function()
+      require('fzf-lua').grep {
+        raw_cmd = [[git diff --name-only | xargs rg --hidden --column --line-number --no-heading --color=always --with-filename -e '']],
+      }
+    end, { desc = 'Grep changed files' })
+  end,
+}
