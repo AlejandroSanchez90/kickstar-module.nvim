6a36dbc7506ca1434329f3f3ef76974995b16234
Alejandro Sanchez <Alejandro_Sanchez@eogresources.com>
2024-12-16 09:16:49 -0600

    updated cofig

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -4,42 +4,42 @@ return {
   event = 'VeryLazy',
   dependencies = {
     'MunifTanjim/nui.nvim',
-    'rcarriga/nvim-notify',
-  },
-  config = function()
-    require('notify').setup {
-      timeout = 2000,
-      background_colour = '#000000',
-      max_width = 50,
-      top_down = false,
-    }
+    {
 
-    require('noice').setup {
-      lsp = {
-        progress = {
+      'rcarriga/nvim-notify',
+      opts = {
+        timeout = 2000,
+        background_colour = '#000000',
+        max_width = 50,
+        top_down = false,
+      },
+    },
+  },
+  opts = {
+    lsp = {
+      progress = {
+        enabled = true,
+      },
+      signature = {
+        enabled = true,
+        auto_open = {
           enabled = true,
-        },
-        signature = {
-          enabled = true,
-          auto_open = {
-            enabled = true,
-            throttle = 2000,
-            trigger = true,
-          },
-        },
-        override = {
-          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-          ['vim.lsp.util.stylize_markdown'] = true,
-          ['cmp.entry.get_documentation'] = true,
+          throttle = 2000,
+          trigger = true,
         },
       },
-      presets = {
-        bottom_search = true,
-        command_palette = true,
-        long_message_to_split = true,
-        inc_rename = false,
-        lsp_doc_border = true,
+      override = {
+        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+        ['vim.lsp.util.stylize_markdown'] = true,
+        ['cmp.entry.get_documentation'] = true,
       },
-    }
-  end,
+    },
+    presets = {
+      bottom_search = true,
+      command_palette = true,
+      long_message_to_split = true,
+      inc_rename = false,
+      lsp_doc_border = true,
+    },
+  },
 }
