df5178cd24844e0e3efa5d48cf9ea7871b6869cc
Alejandro Sanchez <Alejandro_Sanchez@eogresources.com>
2024-12-16 09:53:46 -0600

    better config

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,22 +3,22 @@ return {
   lazy = false,
   name = 'catppuccin',
   priority = 1000,
-  config = function()
-    require('catppuccin').setup {
-      flavour = 'mocha',
-      transparent_background = true,
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-        notify = false,
-        mini = {
-          enabled = true,
-          indentscope_color = '',
-        },
-      },
-    }
+  init = function()
     vim.cmd.colorscheme 'catppuccin'
   end,
+  opts = {
+    flavour = 'mocha',
+    transparent_background = true,
+    integrations = {
+      cmp = true,
+      gitsigns = true,
+      nvimtree = true,
+      treesitter = true,
+      notify = false,
+      mini = {
+        enabled = true,
+        indentscope_color = '',
+      },
+    },
+  },
 }
