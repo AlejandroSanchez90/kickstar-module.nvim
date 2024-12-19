b6d2a23780d77d91ecfa6b9b754697d3d24e3d7f
Alejandro Sanchez <Alejandro_Sanchez@eogresources.com>
2024-12-18 22:25:39 -0600

    auto scroll

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -18,7 +18,7 @@ return {
         question_header = ' Message',
         answer_header = ' Copilot',
         error_header = ' Error',
-        auto_follow_cursor = false,
+        auto_follow_cursor = true,
         context = 'buffer',
       }
 
