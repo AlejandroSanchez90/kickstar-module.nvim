6411ae8435eddb87a36c5dccc5567408c758e1a0
Alejandro Sanchez <Alejandro_Sanchez@eogresources.com>
2024-12-20 14:08:37 -0600

    settings

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -5,14 +5,23 @@ return {
   config = function()
     local fzflua = require 'fzf-lua'
     fzflua.setup {
+      oldfiles = {
+        include_current_session = true,
+      },
       winopts = {
         preview = {
           vertical = 'up:65',
           layout = 'vertical',
         },
       },
+      preview = {
+        builtin = {
+          syntax_limit_b = 1024 * 100,
+        },
+      },
       grep = {
         -- multiline = 1,
+        rg_glob = true,
       },
       keymap = {
         builtin = {
@@ -47,8 +56,8 @@ return {
     keymap.set('n', '<leader>fdd', '<cmd>FzfLua diagnostics_document<cr>', { desc = 'Diagnostic Document' })
     keymap.set('n', '<leader>fdw', '<cmd>FzfLua diagnostics_workspace<cr>', { desc = 'Diagnostic Workspace' })
     keymap.set('n', '<leader>fh', '<cmd>FzfLua oldfiles<cr>', { desc = 'Find recent files' })
-    keymap.set('n', '<leader>fs', '<cmd>FzfLua live_grep_glob --keep-right<cr>', { desc = 'Find string in cwd' })
-    keymap.set('n', '<leader>fc', '<cmd>FzfLua grep_cword rg_glob=true<cr>', { desc = 'Find string under cursor in cwd' })
+    keymap.set('n', '<leader>fs', '<cmd>FzfLua live_grep<cr>', { desc = 'Find string in cwd' })
+    keymap.set('n', '<leader>fc', '<cmd>FzfLua grep_cword <cr>', { desc = 'Find string under cursor in cwd' })
     keymap.set('n', '<leader>fb', '<cmd>FzfLua buffers sort_lastused=false<cr>', { desc = 'Find buffers' })
     keymap.set('n', '<leader><leader>', '<cmd>FzfLua buffers sort_lastused=true<cr>', { desc = 'Find buffers' })
     keymap.set('n', '<leader>fr', '<cmd>FzfLua resume<cr>', { desc = 'Find resume' })
