b35b82e0b433b91bbf75577f50785cc170bf671e
Alejandro Sanchez <Alejandro_Sanchez@eogresources.com>
2024-12-20 14:25:35 -0600

    better color for number column and cursor bg

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -5,6 +5,17 @@ return {
   priority = 1000,
   init = function()
     vim.cmd.colorscheme 'catppuccin'
+
+    -- Line number colors (set after colorscheme)
+    function LineNumberColors()
+      -- Line number colors
+      vim.api.nvim_set_hl(0, 'LineNr', { fg = '#585b70', bold = false })
+      -- Current cursor number
+      vim.api.nvim_set_hl(0, 'CursorLineNr', { bold = true })
+      -- Cursor backgroun line color
+      vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#11111b', bold = false })
+    end
+    LineNumberColors()
   end,
   opts = {
     flavour = 'mocha',
