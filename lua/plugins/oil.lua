eed6b9a2b65a31d843cd32bb085e9dda040774a9
Alejandro Sanchez <Alejandro_Sanchez@eogresources.com>
2025-02-07 13:44:37 -0600

    oil for mini.files

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,20 @@
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {
+    prompt_save_on_select_new_entry = false,
+    skip_confirm_for_simple_edits = true,
+    view_options = {
+      show_hidden = true,
+    },
+  },
+  keys = {
+    { '<leader>e', '<cmd>Oil --float<cr>', mode = { 'n' }, desc = 'File explorer' },
+  },
+  -- Optional dependencies
+  dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
+}
