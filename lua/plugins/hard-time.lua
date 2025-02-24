1ac5f81eb557217418fb20ec92e79d6340898f13
Alejandro Sanchez <contact@alejandrosanchez.dev>
2025-02-23 23:17:13 -0600

    removed unwanted plugins

--- lua/plugins/hard-time.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'm4xshen/hardtime.nvim',
-  dependencies = { 'MunifTanjim/nui.nvim' },
-  opts = {
-    -- :lua print(vim.bo.ft)
-    disabled_filetypes = { 'grug-far', 'minifiles', 'oil', 'trouble' },
-  },
-}
