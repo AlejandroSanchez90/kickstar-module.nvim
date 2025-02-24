51f86284d63e43b2f2652faecae9ec8a3d9cddb1
Alejandro Sanchez <contact@alejandrosanchez.dev>
2025-02-23 23:16:48 -0600

    updated mini to use ys as keymap

--- lua/plugins/mini-surround.lua
+++ lua/plugins/mini-surround.lua
@@ -3,6 +3,18 @@ return {
   event = { 'BufReadPre', 'BufNewFile' },
   version = '*',
   config = function()
-    require('mini.surround').setup()
+    require('mini.surround').setup {
+      mappings = {
+        add = 'ys', -- Add surrounding in Normal and Visual modes
+        delete = 'ysd', -- Delete surrounding
+        find = 'ysf', -- Find surrounding (to the right)
+        find_left = 'ysF', -- Find surrounding (to the left)
+        highlight = 'ysh', -- Highlight surrounding
+        replace = 'ysc', -- Replace surrounding
+        update_n_lines = 'ysn', -- Update `n_lines`
+        suffix_last = 'yl', -- Suffix to search with "prev" method
+        suffix_next = 'yn', -- Suffix to search with "next" method
+      },
+    }
   end,
 }
