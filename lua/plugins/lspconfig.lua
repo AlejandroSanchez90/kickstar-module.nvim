98b569f489ccf2d48cad9994f87bed38616613c9
Alejandro Sanchez <Alejandro_Sanchez@eogresources.com>
2025-05-21 15:04:31 -0500

    new LSP setup

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -18,115 +18,61 @@ return {
       'saghen/blink.cmp',
     },
     config = function()
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-        callback = function(event)
-          local map = function(keys, func, desc, mode)
-            mode = mode or 'n'
-            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-          end
-
-          map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-          map('<leader>d', vim.diagnostic.open_float, 'Show Line Diagnostic')
-          map('[d', vim.diagnostic.goto_prev, 'Previous Diagnostic')
-          map(']d', vim.diagnostic.goto_next, 'Next Diagnostic')
-          map('K', vim.lsp.buf.hover, 'Next Diagnostic')
-          map('<leader>rs', ':LspRestart<CR>', 'Restart LSP')
-          map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
-
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
-          if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-            local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-              buffer = event.buf,
-              group = highlight_augroup,
-              callback = vim.lsp.buf.document_highlight,
-            })
-
-            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-              buffer = event.buf,
-              group = highlight_augroup,
-              callback = vim.lsp.buf.clear_references,
-            })
-
-            vim.api.nvim_create_autocmd('LspDetach', {
-              group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-              callback = function(event2)
-                vim.lsp.buf.clear_references()
-                vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
-              end,
-            })
-          end
-
-          if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-            map('<leader>th', function()
-              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-            end, '[T]oggle Inlay [H]ints')
-          end
-        end,
-      })
-
-      -- Set up diagnostics virtual text
-      vim.diagnostic.config {
-        -- virtual_lines = true,
-        virtual_text = true,
+      require('mason').setup {
+        registries = { 'github:crashdummyy/mason-registry', 'github:mason-org/mason-registry' },
       }
 
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
-
-      local servers = {
-        -- ts_ls = {
-        --   filetypes = { 'typescript', 'typescriptreact', 'javascript', 'javascriptreact' },
-        -- },
-        tailwindcss = { 'typescript', 'typescriptreact', 'javascript', 'javascriptreact' },
-
-        ['emmet_language_server'] = {
-          'html',
-          'typescriptreact',
-          'javascriptreact',
-          'css',
-          'sass',
-          'tailwindcss',
-          'scss',
-          'less',
-          'svelte',
+      require('mason-tool-installer').setup {
+        ensure_installed = {
+          'css-lsp',
+          'emmet-language-server',
+          'eslint_d',
+          'graphql-language-service-cli',
+          'html-lsp',
+          'lua-language-server',
+          'markdownlint',
+          'marksman',
+          'prettier',
+          'prisma-language-server',
+          'stylua',
+          'tailwindcss-language-server',
         },
+      }
 
-        lua_ls = {
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-              diagnostics = { disable = { 'missing-fields' } },
-            },
+      require('mason-lspconfig').setup {}
+
+      local map = function(keys, func, desc, mode)
+        mode = mode or 'n'
+        vim.keymap.set(mode, keys, func, { desc = 'LSP: ' .. desc })
+      end
+
+      map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+      map('<leader>d', vim.diagnostic.open_float, 'Show Line Diagnostic')
+      map('[d', vim.diagnostic.goto_prev, 'Previous Diagnostic')
+      map(']d', vim.diagnostic.goto_next, 'Next Diagnostic')
+      map('K', vim.lsp.buf.hover, 'Next Diagnostic')
+      map('<leader>rs', ':LspRestart<CR>', 'Restart LSP')
+      map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
+
+      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = vim.api.nvim_get_current_buf() })
+      vim.diagnostic.config {
+        signs = {
+          numhl = {
+            [vim.diagnostic.severity.ERROR] = 'DiagnosticSignError',
+            [vim.diagnostic.severity.HINT] = 'DiagnosticSignHint',
+            [vim.diagnostic.severity.INFO] = 'DiagnosticSignInfo',
+            [vim.diagnostic.severity.WARN] = 'DiagnosticSignWarn',
+          },
+          text = {
+            [vim.diagnostic.severity.ERROR] = 'X',
+            [vim.diagnostic.severity.HINT] = '?',
+            [vim.diagnostic.severity.INFO] = 'I',
+            [vim.diagnostic.severity.WARN] = '!',
           },
         },
-      }
-
-      require('mason').setup()
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua',
-        'html',
-        'cssls',
-        'tailwindcss',
-        'lua_ls',
-        'emmet_language_server',
-        'prismals',
-        'eslint_d',
-        'prettier',
-      })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-      require('mason-lspconfig').setup {
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            server.capabilities = capabilities
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
+        update_in_insert = true,
+        virtual_text = { current_line = true },
+        -- virtual_lines = { current_line = false },
       }
     end,
   },
