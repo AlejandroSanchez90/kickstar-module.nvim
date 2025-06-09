9e6145a1a2ec5b201b4a742cc4c64d2b53635b79
Alejandro Sanchez <contact@alejandrosanchez.dev>
2025-06-08 21:38:15 -0500

    changed mini pairs for autopairs

---
+++ lua/plugins/nvim-autopairs.lua
@@ -0,0 +1,8 @@
+-- return { 'echasnovski/mini.pairs', version = false, opts = {} }
+return {
+  'windwp/nvim-autopairs',
+  event = 'InsertEnter',
+  config = true,
+  -- use opts = {} for passing setup options
+  -- this is equivalent to setup({}) function
+}
