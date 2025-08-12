1295c3654df11ea7d7d926e1cee570e369c277ab
Alejandro Sanchez <Alejandro_Sanchez@eogresources.com>
2025-08-12 11:22:24 -0500

    added outline for symbols (g-s)

--- lua/plugins/outline.lua
+++ lua/plugins/outline.lua
@@ -2,12 +2,23 @@ return {
   'hedyhli/outline.nvim',
   config = function()
     -- Example mapping to toggle outline
-    vim.keymap.set('n', '<leader>fo', '<cmd>Outline<CR>', { desc = 'Toggle Outline' })
+    vim.keymap.set('n', 'gs', '<cmd>Outline<CR>', { desc = 'Toggle Outline' })
 
+    print 'worked'
     require('outline').setup {
+
+      outline_window = {
+        position = 'left',
+        width = 20,
+
+        show_numbers = true,
+        show_relative_numbers = true,
+      },
       symbol_folding = {
+        autofold_depth = 1,
         auto_unfold = {
-          only = 1,
+          only = true,
+          -- hovered = false,
         },
       },
       -- Your setup opts here (leave empty to use defaults)
