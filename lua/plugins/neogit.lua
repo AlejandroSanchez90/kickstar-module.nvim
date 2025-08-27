60a94e6ce30f8cc67b7f66b2f7d6b5a0670b8ca6
Alejandro Sanchez <Alejandro_Sanchez@eogresources.com>
2025-08-27 10:16:20 -0500

    close diffview with q

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -5,9 +5,23 @@ return {
       'sindrets/diffview.nvim',
       config = function()
         require('diffview').setup {
+          keymaps = {
+            view = {
+              ['q'] = '<Cmd>DiffviewClose<CR>',
+            },
+            file_panel = {
+              ['q'] = '<Cmd>DiffviewClose<CR>',
+            },
+            file_history_panel = {
+              ['q'] = '<Cmd>DiffviewClose<CR>',
+            },
+            option_panel = {
+              ['q'] = '<Cmd>DiffviewClose<CR>',
+            },
+          },
           view = {
             merge_tool = {
-              layout = 'diff3_mixed', -- 2 horizontal, main vertical
+              layout = 'diff4_mixed', -- 2 horizontal, main vertical
             },
           },
         }
