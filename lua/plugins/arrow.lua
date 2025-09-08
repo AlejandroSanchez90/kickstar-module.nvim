a175d89fe0b8106dc299ae7a53294838b025bfdf
Alejandro Sanchez <asanchez3@houasanchez3-mbQ659.eogresources.com>
2025-09-08 10:21:03 -0500

    update arrow

--- lua/plugins/arrow.lua
+++ lua/plugins/arrow.lua
@@ -3,10 +3,20 @@ return {
   jhdependencies = {
     { 'nvim-tree/nvim-web-devicons' },
   },
-  opts = {
-    show_icons = true,
-    leader_key = "'",
-    buffer_leader_key = 'm',
-    separate_by_branch = true,
-  },
+
+  config = function()
+    local arrow = require 'arrow'
+    arrow.setup {
+      show_icons = true,
+      leader_key = "'",
+      buffer_leader_key = 'm',
+      separate_by_branch = true,
+      per_buffer_config = {
+        lines = 1, -- Number of lines showed on preview.
+      },
+    }
+    vim.keymap.set('n', 'H', require('arrow.persist').previous)
+    vim.keymap.set('n', 'L', require('arrow.persist').next)
+    vim.keymap.set('n', '<C-s>', require('arrow.persist').toggle)
+  end,
 }
