a54f01d38bf848b222fbdfe37acfa9a1701db570
Alejandro Sanchez <asanchez3@houasanchez3-mbQ659.eogresources.com>
2025-09-09 10:42:08 -0500

    arrow keymaps and arrow in status line

--- lua/plugins/arrow.lua
+++ lua/plugins/arrow.lua
@@ -17,6 +17,6 @@ return {
     }
     vim.keymap.set('n', 'H', require('arrow.persist').previous)
     vim.keymap.set('n', 'L', require('arrow.persist').next)
-    vim.keymap.set('n', '<C-s>', require('arrow.persist').toggle)
+    vim.keymap.set('n', 'M', require('arrow.persist').toggle)
   end,
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -12,10 +12,10 @@ return {
       return 'Macro: ' .. reg
     end
 
+    local arrow_status = require 'arrow.statusline'
     local function isRecordingActive()
       return vim.fn.reg_recording() ~= ''
     end
-
     lualine.setup {
       options = {
         theme = 'catppuccin',
@@ -34,10 +34,23 @@ return {
             color = { fg = '#ff9e64' },
           },
         },
-        lualine_c = { { 'filename', path = 1, file_status = true } },
+        lualine_c = {
+          { 'filename', path = 0, file_status = true, color = { gui = 'bold' } },
+
+          -- arrow per file
+          {
+            function()
+              return arrow_status.text_for_statusline_with_icons()
+            end,
+            cond = function()
+              return arrow_status.is_on_arrow_file() ~= nil
+            end,
+            color = { fg = '#51cf66', gui = 'bold' },
+          },
+        },
       },
       inactive_sections = {
-        lualine_c = { { 'filename', path = 1, file_status = true } },
+        lualine_c = { { 'filename', path = 0, file_status = true } },
       },
     }
   end,
