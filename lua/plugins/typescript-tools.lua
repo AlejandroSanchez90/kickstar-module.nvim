b21c2776474c3380bbfbb3c7a5188378bc7f9365
Alejandro Sanchez <contact@alejandrosanchez.dev>
2025-10-01 12:43:25 -0500

    fix eslint and typescript code actions

--- lua/plugins/typescript-tools.lua
+++ lua/plugins/typescript-tools.lua
@@ -5,40 +5,6 @@ return {
     'nvim-lua/plenary.nvim',
     'neovim/nvim-lspconfig',
   },
-  opts = {
-    settings = {
-      -- Performance optimizations for large projects
-      tsserver_max_memory = 8192,
-      tsserver_plugins = {},
-      -- Exclude node_modules and other heavy directories
-      tsserver_file_preferences = {
-        includeCompletionsForModuleExports = true,
-        includeCompletionsForImportStatements = true,
-        includeInlayParameterNameHints = 'literals',
-        includeInlayFunctionParameterTypeHints = false,
-        includeInlayVariableTypeHints = false,
-        includeInlayPropertyDeclarationTypeHints = false,
-        includeInlayFunctionLikeReturnTypeHints = false,
-        includeInlayEnumMemberValueHints = false,
-      },
-      -- Exclude patterns for file watching
-      watchOptions = {
-        excludeDirectories = {
-          '**/node_modules',
-          '**/.git',
-          '**/dist',
-          '**/build',
-          '**/.next',
-          '**/coverage',
-          '**/.nyc_output',
-        },
-      },
-      -- TypeScript server options
-      typescript = {
-        preferences = {
-          includePackageJsonAutoImports = 'off',
-        },
-      },
-    },
-  },
+  opts = {},
 }
+
