f8a507c1c398f09bb0c0ec276554943d9339f223
Alejandro Sanchez <contact@alejandrosanchez.dev>
2025-10-27 08:47:00 -0500

    updated leap

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,13 +1,25 @@
 return {
   'ggandor/leap.nvim',
   config = function()
-    require('leap').add_default_mappings()
+    vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
+    vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
     vim.keymap.set({ 'n', 'o' }, 'g/', function()
       require('leap.remote').action { jumper = '/' }
     end)
     vim.keymap.set({ 'n', 'o' }, 'g?', function()
       require('leap.remote').action { jumper = '?' }
     end)
+    require('leap').opts.preview = function(ch0, ch1, ch2)
+      return not (ch1:match '%s' or (ch0:match '%a' and ch1:match '%a' and ch2:match '%a'))
+    end
+    require('leap.user').set_repeat_keys('<enter>', '<backspace>')
+
+    require('leap').opts.equivalence_classes = {
+      ' \t\r\n',
+      '([{',
+      ')]}',
+      '\'"`',
+    }
   end,
   lazy = false,
 }
