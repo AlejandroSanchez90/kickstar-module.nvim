478aeaa2ef21c7747e82b802ac61e78109822600
Alejandro Sanchez <contact@alejandrosanchez.dev>
2025-11-06 13:13:02 -0600

    open trouble when qflist

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -2,7 +2,28 @@ return {
   'folke/trouble.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons', 'folke/todo-comments.nvim' },
   cmd = 'Trouble',
+
   opts = {},
+  specs = {
+    'folke/snacks.nvim',
+    opts = function(_, opts)
+      return vim.tbl_deep_extend('force', opts or {}, {
+        picker = {
+          actions = require('trouble.sources.snacks').actions,
+          win = {
+            input = {
+              keys = {
+                ['<c-q>'] = {
+                  'trouble_open',
+                  mode = { 'n', 'i' },
+                },
+              },
+            },
+          },
+        },
+      })
+    end,
+  },
   keys = {
     { '<leader>xx', '<cmd>Trouble diagnostics toggle focus=true<cr>', desc = 'Open/close trouble list' },
     {
